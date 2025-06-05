e2aef637b9b070561ccd41599c696d0ffafd4045
Vishal Choudhary <vishchy111@gmail.com>
2025-06-05 17:06:25 +0530

    Practice Revision Project

---
+++ src/main/java/PracticeProject/SecurityOauthRegister/SecurityOauthRegisterApplication.java
@@ -0,0 +1,13 @@
+package PracticeProject.SecurityOauthRegister;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityOauthRegisterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityOauthRegisterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/PracticeProject/SecurityOauthRegister/SecurityOauthRegisterApplicationTests.java
@@ -0,0 +1,13 @@
+package PracticeProject.SecurityOauthRegister;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityOauthRegisterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
